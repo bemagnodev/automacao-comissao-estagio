731958f9615f51ab95918e4bc74d308eb645d51f
Bernardo <bemagno13@gmail.com>
2025-09-14 22:21:17 -0300

    remoção temporaria de logica de verificação de empresas

--- src/main.py
+++ src/main.py
@@ -57,7 +57,7 @@ def generate_report_card(academic_data, validations_dict):
                 "valid_cr": {"name": "Coeficiente de Rendimento", "value": academic_data.get("cr_acumulado"), "required": academic_requirements["minimum_cr"]},
                 "valid_periods": {"name": "Períodos Cursados", "value": academic_data.get("periodos_integralizados"), "required": f'<= {academic_requirements["max_periods"]}'},
                 "valid_ext_hours": {"name": "Horas de Extensão", "value": academic_data.get("carga_horaria_extensao"), "required": f">= {academic_requirements['minimum_ext_hours']}"},
-                "valid_company": {"name": "Empresa Conveniada", "value": validations_dict.get("valid_company"), "required": "True"},
+                # "valid_company": {"name": "Empresa Conveniada", "value": validations_dict.get("valid_company"), "required": "True"},
                 "valid_courses": {"name": "Disciplinas Obrigatórias", "value": validations_dict.get("valid_courses"), "required": "True"},
             }
 
@@ -127,7 +127,7 @@ def main():
     # --- Componentes da Interface ---
 
     # 1. Componente de Upload de Arquivo (PDF)
-    st.header("1. Faça o upload do seu BOA em PDF")
+    st.header("Faça o upload do seu BOA em PDF")
     uploaded_file = st.file_uploader(
         "Escolha o arquivo PDF",
         type="pdf",
@@ -140,43 +140,44 @@ def main():
         else:
             st.error("O arquivo carregado não é um BOA válido. Por favor, verifique o arquivo e tente novamente.")
 
-    # 2. Componente de Seleção (Selectbox)
-    st.header("2. Selecione a Empresa Conveniada")
+    # # 2. Componente de Seleção (Selectbox)
+    # st.header("2. Selecione a Empresa Conveniada")
 
-    # Lista das empresas conveniadas
-    empresas_conveniadas = companies_df["INSTITUIÇÃO"].tolist()
+    # # Lista das empresas conveniadas
+    # empresas_conveniadas = companies_df["INSTITUIÇÃO"].tolist()
 
-    # O widget selectbox retorna o nome que foi selecionado
-    selected_company = st.selectbox(
-        "Escolha uma empresa abaixo:",
-        options=empresas_conveniadas,
-        index=None,
-        placeholder="Selecione uma das empresas conveniadas à UFRJ",
-        help="O nome selecionado será associado ao documento enviado."
-    )
+    # # O widget selectbox retorna o nome que foi selecionado
+    # selected_company = st.selectbox(
+    #     "Escolha uma empresa abaixo:",
+    #     options=empresas_conveniadas,
+    #     index=None,
+    #     placeholder="Selecione uma das empresas conveniadas à UFRJ",
+    #     help="O nome selecionado será associado ao documento enviado."
+    # )
 
-    st.write("#### Não encontrou a empresa?")
+    # st.write("#### Não encontrou a empresa?")
 
-    # 2. Crie o checkbox para ativar a entrada manual
-    ativar_entrada_manual = st.checkbox("Digitar o nome de outra empresa")
+    # # 2. Crie o checkbox para ativar a entrada manual
+    # ativar_entrada_manual = st.checkbox("Digitar o nome de outra empresa")
 
-    # 3. Variável para armazenar o nome final da empresa
-    nome_empresa_final = ""
+    # # 3. Variável para armazenar o nome final da empresa
+    # nome_empresa_final = ""
 
-    # 4. Lógica condicional baseada no checkbox
-    if ativar_entrada_manual:
-        # Se o checkbox estiver MARCADO, mostre o campo de texto
-        nome_digitado = st.text_input(
-            "Digite o nome da nova empresa:",
-            placeholder="Nome da empresa que não está na lista"
-        )
-        # O nome final será o que o usuário digitar
-        nome_empresa_final = nome_digitado.strip()
-    else:
-        # Se o checkbox estiver DESMARCADO, o nome final é o do selectbox
-        nome_empresa_final = selected_company
+    # # 4. Lógica condicional baseada no checkbox
+    # if ativar_entrada_manual:
+    #     # Se o checkbox estiver MARCADO, mostre o campo de texto
+    #     nome_digitado = st.text_input(
+    #         "Digite o nome da nova empresa:",
+    #         placeholder="Nome da empresa que não está na lista"
+    #     )
+    #     # O nome final será o que o usuário digitar
+    #     nome_empresa_final = nome_digitado.strip()
+    # else:
+    #     # Se o checkbox estiver DESMARCADO, o nome final é o do selectbox
+    #     nome_empresa_final = selected_company
+    # st.markdown("---")
 
-    st.markdown("---")
+    nome_empresa_final = "2PLAN STUDIO ARQUITETURA LTDA"
 
     # --- Lógica e Exibição dos Resultados ---
     if st.button("Processar Dados"):
